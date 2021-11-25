d00a8abfe8df7f6829f0aad1d51d9c90c5c3d515
Janghyun1230 <kimjanghyun1230@gmail.com>
2021-11-25 13:32:24 +0900

    clean up

--- imagenet/test.py
+++ imagenet/test.py
@@ -118,14 +118,11 @@ def main():
     elif args.dataset == 'imagenet':
         valdir = os.path.join('/data_large/readonly/ImageNet-Fast/imagenet/val')
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
-        val_loader = torch.utils.data.DataLoader(datasets.ImageFolder(
-            valdir,
-            transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(), normalize
-            ])),
+        val_transform = transforms.Compose(
+            [transforms.Resize(256),
+             transforms.CenterCrop(224),
+             transforms.ToTensor(), normalize])
+        val_loader = torch.utils.data.DataLoader(datasets.ImageFolder(valdir, val_transform),
                                                  batch_size=args.batch_size,
                                                  shuffle=False,
                                                  num_workers=args.workers,
