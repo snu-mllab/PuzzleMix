f61efea4c6cba389ca74bfd18398093222ebebb9
Janghyun1230 <kimjanghyun1230@gmail.com>
2021-11-25 13:26:43 +0900

    clean up

--- load_data.py
+++ load_data.py
@@ -7,10 +7,15 @@ from functools import reduce
 from operator import __or__
 
 
-def load_data_subset(batch_size,workers,dataset, data_target_dir, labels_per_class=100, valid_labels_per_class = 500, mixup_alpha=1):
+def load_data_subset(batch_size,
+                     workers,
+                     dataset,
+                     data_target_dir,
+                     labels_per_class=100,
+                     valid_labels_per_class=500,
+                     mixup_alpha=1):
     '''return datalaoder'''
     ## copied from GibbsNet_pytorch/load.py
-            
     if dataset == 'cifar10':
         mean = [x / 255 for x in [125.3, 123.0, 113.9]]
         std = [x / 255 for x in [63.0, 62.1, 66.7]]
@@ -22,59 +27,81 @@ def load_data_subset(batch_size,workers,dataset, data_target_dir, labels_per_cla
         std = [x / 255 for x in [127.5, 127.5, 127.5]]
     else:
         assert False, "Unknow dataset : {}".format(dataset)
-    
+
     # pre-processing
     if dataset == 'tiny-imagenet-200':
-        train_transform = transforms.Compose(
-                [transforms.RandomHorizontalFlip(),
-                 transforms.RandomCrop(64, padding=4),
-                 transforms.ToTensor(),
-                 transforms.Normalize(mean, std)])
-        
+        train_transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(64, padding=4),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
         test_transform = transforms.Compose(
-                [transforms.ToTensor(), transforms.Normalize(mean, std)])
-    else:    
-        train_transform = transforms.Compose(
-                [transforms.RandomHorizontalFlip(),
-                 transforms.RandomCrop(32, padding=2),
-                 transforms.ToTensor(),
-                 transforms.Normalize(mean, std)])
-       
+            [transforms.ToTensor(), transforms.Normalize(mean, std)])
+    else:
+        train_transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
         test_transform = transforms.Compose(
-                [transforms.ToTensor(), transforms.Normalize(mean, std)])
-    
+            [transforms.ToTensor(), transforms.Normalize(mean, std)])
+
     # dataset
     if dataset == 'cifar10':
-        train_data = datasets.CIFAR10(data_target_dir, train=True, transform=train_transform, download=True)
-        test_data = datasets.CIFAR10(data_target_dir, train=False, transform=test_transform, download=True)
+        train_data = datasets.CIFAR10(data_target_dir,
+                                      train=True,
+                                      transform=train_transform,
+                                      download=True)
+        test_data = datasets.CIFAR10(data_target_dir,
+                                     train=False,
+                                     transform=test_transform,
+                                     download=True)
         num_classes = 10
     elif dataset == 'cifar100':
-        train_data = datasets.CIFAR100(data_target_dir, train=True, transform=train_transform, download=True)
-        test_data = datasets.CIFAR100(data_target_dir, train=False, transform=test_transform, download=True)
+        train_data = datasets.CIFAR100(data_target_dir,
+                                       train=True,
+                                       transform=train_transform,
+                                       download=True)
+        test_data = datasets.CIFAR100(data_target_dir,
+                                      train=False,
+                                      transform=test_transform,
+                                      download=True)
         num_classes = 100
     elif dataset == 'tiny-imagenet-200':
-        train_root = os.path.join(data_target_dir, 'train')  # this is path to training images folder
-        validation_root = os.path.join(data_target_dir, 'val/images')  # this is path to validation images folder
+        train_root = os.path.join(data_target_dir,
+                                  'train')  # this is path to training images folder
+        validation_root = os.path.join(data_target_dir,
+                                       'val/images')  # this is path to validation images folder
         train_data = datasets.ImageFolder(train_root, transform=train_transform)
-        test_data = datasets.ImageFolder(validation_root,transform=test_transform)
+        test_data = datasets.ImageFolder(validation_root, transform=test_transform)
         num_classes = 200
     else:
         assert False, 'Do not support dataset : {}'.format(dataset)
-        
+
     n_labels = num_classes
-    
+
     # random sampler
-    def get_sampler(labels, n=None, n_valid= None):
+    def get_sampler(labels, n=None, n_valid=None):
         # Only choose digits in n_labels
         # n = number of labels per class for training
         # n_val = number of lables per class for validation
-        (indices,) = np.where(reduce(__or__, [labels == i for i in np.arange(n_labels)]))
+        (indices, ) = np.where(reduce(__or__, [labels == i for i in np.arange(n_labels)]))
         np.random.shuffle(indices)
-        
-        indices_valid = np.hstack([list(filter(lambda idx: labels[idx] == i, indices))[:n_valid] for i in range(n_labels)])
-        indices_train = np.hstack([list(filter(lambda idx: labels[idx] == i, indices))[n_valid:n_valid+n] for i in range(n_labels)])
-        indices_unlabelled = np.hstack([list(filter(lambda idx: labels[idx] == i, indices))[:] for i in range(n_labels)])
- 
+
+        indices_valid = np.hstack([
+            list(filter(lambda idx: labels[idx] == i, indices))[:n_valid] for i in range(n_labels)
+        ])
+        indices_train = np.hstack([
+            list(filter(lambda idx: labels[idx] == i, indices))[n_valid:n_valid + n]
+            for i in range(n_labels)
+        ])
+        indices_unlabelled = np.hstack(
+            [list(filter(lambda idx: labels[idx] == i, indices))[:] for i in range(n_labels)])
+
         indices_train = torch.from_numpy(indices_train)
         indices_valid = torch.from_numpy(indices_valid)
         indices_unlabelled = torch.from_numpy(indices_unlabelled)
@@ -82,24 +109,52 @@ def load_data_subset(batch_size,workers,dataset, data_target_dir, labels_per_cla
         sampler_valid = SubsetRandomSampler(indices_valid)
         sampler_unlabelled = SubsetRandomSampler(indices_unlabelled)
         return sampler_train, sampler_valid, sampler_unlabelled
-    
+
     if dataset == 'tiny-imagenet-200':
         pass
     else:
-        train_sampler, valid_sampler, unlabelled_sampler = get_sampler(train_data.targets, labels_per_class, valid_labels_per_class)
+        train_sampler, valid_sampler, unlabelled_sampler = get_sampler(
+            train_data.targets, labels_per_class, valid_labels_per_class)
 
     # dataloader
     if dataset == 'tiny-imagenet-200':
-        labelled = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+        labelled = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=workers,
+                                               pin_memory=True)
         validation = None
         unlabelled = None
-        test = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True)
+        test = torch.utils.data.DataLoader(test_data,
+                                           batch_size=batch_size,
+                                           shuffle=False,
+                                           num_workers=workers,
+                                           pin_memory=True)
     else:
-        labelled = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler = train_sampler, shuffle=False, num_workers=workers, pin_memory=True)
-        validation = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler = valid_sampler, shuffle=False, num_workers=workers, pin_memory=True)
-        unlabelled = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler = unlabelled_sampler, shuffle=False, num_workers=workers, pin_memory=True)
-        test = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True)
-    
+        labelled = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=train_sampler,
+                                               shuffle=False,
+                                               num_workers=workers,
+                                               pin_memory=True)
+        validation = torch.utils.data.DataLoader(train_data,
+                                                 batch_size=batch_size,
+                                                 sampler=valid_sampler,
+                                                 shuffle=False,
+                                                 num_workers=workers,
+                                                 pin_memory=True)
+        unlabelled = torch.utils.data.DataLoader(train_data,
+                                                 batch_size=batch_size,
+                                                 sampler=unlabelled_sampler,
+                                                 shuffle=False,
+                                                 num_workers=workers,
+                                                 pin_memory=True)
+        test = torch.utils.data.DataLoader(test_data,
+                                           batch_size=batch_size,
+                                           shuffle=False,
+                                           num_workers=workers,
+                                           pin_memory=True)
+
     return labelled, validation, unlabelled, test, num_classes
 
 
@@ -111,9 +166,10 @@ def create_val_folder(data_set_path):
     so that test and val data can be read by the pytorch dataloaders
     """
     path = os.path.join(data_set_path, 'val/images')  # path where validation data is present now
-    filename = os.path.join(data_set_path, 'val/val_annotations.txt')  # file where image2class mapping is present
-    fp = open(filename, "r")   
-    data = fp.readlines() 
+    filename = os.path.join(data_set_path,
+                            'val/val_annotations.txt')  # file where image2class mapping is present
+    fp = open(filename, "r")
+    data = fp.readlines()
 
     # Create a dictionary with image names as key and corresponding classes as values
     val_img_dict = {}
@@ -133,6 +189,4 @@ def create_val_folder(data_set_path):
 
 
 if __name__ == "__main__":
-    create_val_folder('data/tiny-imagenet-200') 
-
-
+    create_val_folder('data/tiny-imagenet-200')
