24dc5b6013d6fbbecbc0761f3caf460dc541de84
Janghyun1230 <kimjanghyun1230@gmail.com>
2021-11-25 13:27:10 +0900

    clean up

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import sys
 if sys.version_info[0] < 3:
     import cPickle as pickle
 else:
- import _pickle as pickle
+    import _pickle as pickle
 import os
 import shutil
 import numpy as np
@@ -21,15 +21,15 @@ def copy_script_to_folder(caller_path, folder):
 
 def time_string():
     '''convert time format'''
-    ISOTIMEFORMAT='%Y-%m-%d %X'
-    string = '[{}]'.format(time.strftime( ISOTIMEFORMAT, time.gmtime(time.time()) ))
+    ISOTIMEFORMAT = '%Y-%m-%d %X'
+    string = '[{}]'.format(time.strftime(ISOTIMEFORMAT, time.gmtime(time.time())))
     return string
 
 
 def convert_secs2time(epoch_time):
     need_hour = int(epoch_time / 3600)
-    need_mins = int((epoch_time - 3600*need_hour) / 60)
-    need_secs = int(epoch_time - 3600*need_hour - 60*need_mins)
+    need_mins = int((epoch_time - 3600 * need_hour) / 60)
+    need_secs = int(epoch_time - 3600 * need_hour - 60 * need_mins)
     return need_hour, need_mins, need_secs
 
 
@@ -40,18 +40,20 @@ class RecorderMeter(object):
 
     def reset(self, total_epoch):
         assert total_epoch > 0
-        self.total_epoch   = total_epoch
+        self.total_epoch = total_epoch
         self.current_epoch = 0
-        self.epoch_losses  = np.zeros((self.total_epoch, 2), dtype=np.float32) # [epoch, train/val]
-        self.epoch_losses  = self.epoch_losses - 1
+        self.epoch_losses = np.zeros((self.total_epoch, 2), dtype=np.float32)  # [epoch, train/val]
+        self.epoch_losses = self.epoch_losses - 1
 
-        self.epoch_accuracy= np.zeros((self.total_epoch, 2), dtype=np.float32) # [epoch, train/val]
-        self.epoch_accuracy= self.epoch_accuracy
+        self.epoch_accuracy = np.zeros((self.total_epoch, 2),
+                                       dtype=np.float32)  # [epoch, train/val]
+        self.epoch_accuracy = self.epoch_accuracy
 
     def update(self, idx, train_loss, train_acc, val_loss, val_acc):
-        assert idx >= 0 and idx < self.total_epoch, 'total_epoch : {} , but update with the {} index'.format(self.total_epoch, idx)
-        self.epoch_losses  [idx, 0] = train_loss
-        self.epoch_losses  [idx, 1] = val_loss
+        assert idx >= 0 and idx < self.total_epoch, 'total_epoch : {} , but update with the {} index'.format(
+            self.total_epoch, idx)
+        self.epoch_losses[idx, 0] = train_loss
+        self.epoch_losses[idx, 1] = val_loss
         self.epoch_accuracy[idx, 0] = train_acc
         self.epoch_accuracy[idx, 1] = val_acc
         self.current_epoch = idx + 1
@@ -60,18 +62,18 @@ class RecorderMeter(object):
     def max_accuracy(self, istrain):
         if self.current_epoch <= 0: return 0
         if istrain: return self.epoch_accuracy[:self.current_epoch, 0].max()
-        else:       return self.epoch_accuracy[:self.current_epoch, 1].max()
-  
+        else: return self.epoch_accuracy[:self.current_epoch, 1].max()
+
     def plot_curve(self, save_path):
         title = 'the accuracy/loss curve of train/val'
-        dpi = 80  
+        dpi = 80
         width, height = 1200, 800
         legend_fontsize = 10
         scale_distance = 48.8
         figsize = width / float(dpi), height / float(dpi)
 
         fig = plt.figure(figsize=figsize)
-        x_axis = np.array([i for i in range(self.total_epoch)]) # epochs
+        x_axis = np.array([i for i in range(self.total_epoch)])  # epochs
         y_axis = np.zeros(self.total_epoch)
 
         plt.xlim(0, self.total_epoch)
@@ -84,7 +86,7 @@ class RecorderMeter(object):
         plt.title(title, fontsize=20)
         plt.xlabel('the training epoch', fontsize=16)
         plt.ylabel('accuracy', fontsize=16)
-      
+
         y_axis[:] = self.epoch_accuracy[:, 0]
         plt.plot(x_axis, y_axis, color='g', linestyle='-', label='train-accuracy', lw=2)
         plt.legend(loc=4, fontsize=legend_fontsize)
@@ -93,18 +95,17 @@ class RecorderMeter(object):
         plt.plot(x_axis, y_axis, color='y', linestyle='-', label='valid-accuracy', lw=2)
         plt.legend(loc=4, fontsize=legend_fontsize)
 
-        
         y_axis[:] = self.epoch_losses[:, 0]
-        plt.plot(x_axis, y_axis*50, color='g', linestyle=':', label='train-loss-x50', lw=2)
+        plt.plot(x_axis, y_axis * 50, color='g', linestyle=':', label='train-loss-x50', lw=2)
         plt.legend(loc=4, fontsize=legend_fontsize)
 
         y_axis[:] = self.epoch_losses[:, 1]
-        plt.plot(x_axis, y_axis*50, color='y', linestyle=':', label='valid-loss-x50', lw=2)
+        plt.plot(x_axis, y_axis * 50, color='y', linestyle=':', label='valid-loss-x50', lw=2)
         plt.legend(loc=4, fontsize=legend_fontsize)
 
         if save_path is not None:
             fig.savefig(save_path, dpi=dpi, bbox_inches='tight')
-            print ('---- save figure {} into {}'.format(title, save_path))
+            print('---- save figure {} into {}'.format(title, save_path))
         plt.close(fig)
 
 
@@ -132,31 +133,30 @@ def plotting(exp_dir):
     ###########################################################
     ### Make the vanilla train and test loss per epoch plot ###
     ###########################################################
-   
+
     plt.plot(np.asarray(train_dict['train_loss']), label='train_loss')
     plt.plot(np.asarray(train_dict['test_loss']), label='test_loss')
-        
+
     #plt.ylim(0,2000)
     plt.xlabel('evaluation step')
     plt.ylabel('metrics')
     plt.tight_layout()
     plt.legend(loc='upper right')
-    plt.savefig(os.path.join(exp_dir, 'loss.png' ))
+    plt.savefig(os.path.join(exp_dir, 'loss.png'))
     plt.clf()
-   
+
     ## accuracy###
     plt.plot(np.asarray(train_dict['train_acc']), label='train_acc')
     plt.plot(np.asarray(train_dict['test_acc']), label='test_acc')
-        
+
     #plt.ylim(0,2000)
     plt.xlabel('evaluation step')
     plt.ylabel('metrics')
     plt.tight_layout()
     plt.legend(loc='upper right')
-    plt.savefig(os.path.join(exp_dir, 'acc.png' ))
+    plt.savefig(os.path.join(exp_dir, 'acc.png'))
     plt.clf()
-    
-   
+
+
 if __name__ == '__main__':
     plotting('temop')
-
